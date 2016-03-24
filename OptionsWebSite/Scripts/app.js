faf9a29fa5e590205a3582dec6c0e157cb853212
TrevorBroderick <Broderick.Trevor@gmail.com>
2016-03-24 14:55:37 -0700

    AngularJS Starter added

---
+++ OptionsWebSite/Scripts/app.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    var app = angular.module('reports', []);
+
+    app.controller('ReportsController', function ($scope, $http) {
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        };
+        var onError = function (error) {
+            $scope.error = error;
+        };
+
+        $http.get("http://localhost:59788/api/Choices")
+        .then(onUserComplete, onError);
+        $scope.message = "Hello, Angular!";
+    });
+})();
